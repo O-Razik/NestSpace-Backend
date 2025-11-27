472290507be1d347fa616e0acf0e081d2da31883
O-Razik <razik8bit@gmail.com>
2025-11-27 15:40:55 +0200

    Added IRepositories

---
+++ src/EventScheduleService/EventScheduleService.ABS/IRepositories/IEventTagRepository.cs
@@ -0,0 +1,16 @@
+using EventScheduleService.ABS.IModels;
+
+namespace EventScheduleService.ABS.IRepositories;
+
+public interface IEventTagRepository
+{
+    Task<IEnumerable<IEventTag>> GetBySpaceAsync(Guid spaceId);
+    
+    Task<IEventTag?> GetByIdAsync(Guid markerId);
+    
+    Task<IEventTag> AddAsync(IEventTag newTag);
+    
+    Task<IEventTag?> UpdateAsync(IEventTag updatedTag);
+    
+    Task<bool> DeleteAsync(Guid markerId);
+}
